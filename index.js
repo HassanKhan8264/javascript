3fc5320844fc1d54d95ec96f8933b50027123176
HassanKhan8264 <HF610061@GMAIL.COM>
2024-05-31 04:29:55 +0500

    some js

---
+++ index.js
@@ -0,0 +1,11 @@
+login = async (value) => {
+    console.log('thisis the them testgin', value);
+}
+login('hassan')
+
+let js = 'amazing';
+if (js === 'amazing') {
+    alert('amazing')
+}
+let Number = 56 + 56 + 56 - 7
+console.log(Number);
