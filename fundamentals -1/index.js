3e09288aca9b09837d24af0b432e0c54f5038d8e
HassanKhan8264 <HF610061@GMAIL.COM>
2024-06-05 03:56:47 +0500

    lec 13 done

--- fundamentals -1/index.js
+++ fundamentals -1/index.js
@@ -5,7 +5,10 @@ login('hassan')
 
 let js = 'amazing';
 if (js === 'amazing') {
+    let js = "amazing 2"
     alert('amazing')
+    console.log('in scope', js);
 }
+console.log('out scope', js);
 let Number = 56 + 56 + 56 - 7
 console.log(Number);
