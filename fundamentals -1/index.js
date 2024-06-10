29d75cdf909ccd117e620f4a4fb7f62b2f219b6f
HassanKhan8264 <HF610061@GMAIL.COM>
2024-06-11 01:19:31 +0500

    type conversion operators precedence

--- fundamentals -1/index.js
+++ fundamentals -1/index.js
@@ -12,3 +12,18 @@ if (js === 'amazing') {
 console.log('out scope', js);
 let Number = 56 + 56 + 56 - 7
 console.log(Number);
+/* Write your code below. Good luck! 🙂 */
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJhon = 1.95;
+
+let BMIMark = massMark / (heightMark * heightMark)
+let BMIJohn = massJohn / (heightJhon * heightJhon)
+console.log('mass', BMIJohn, BMIMark)
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+}
