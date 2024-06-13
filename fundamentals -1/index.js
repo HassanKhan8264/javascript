3b43acb22e54b4e72dc38d043ffabf68a1810405
HassanKhan8264 <HF610061@GMAIL.COM>
2024-06-14 04:48:16 +0500

    reducr filter methods

--- fundamentals -1/index.js
+++ fundamentals -1/index.js
@@ -27,3 +27,48 @@ if (BMIMark > BMIJohn) {
 } else {
     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
 }
+
+
+// there is 5 falsy values in js 0 ,'', undefined, null,NaN
+// and truthy values are {}, 'string'
+function falsyTruthyValues() {
+    let object = {}
+    if (object) {
+        return !!object
+    }
+}
+console.log(falsyTruthyValues());
+
+
+// map returns a new array
+// filter returns a new array with expected values
+// reduce returns a element with each item added values
+
+
+
+/* Write your code below. Good luck! 🙂 */
+
+// Scores for Dolphins
+const dolphinScores = [96, 108, 89];
+
+// Scores for Koalas
+const koalaScores = [88, 91, 110];
+
+// Function to calculate the average score
+function calculateAverageScore(scores) {
+    const totalScore = scores.reduce((a, b) => a + b, 0);
+    return totalScore / scores.length;
+}
+
+// Calculate average scores
+const scoreDolphins = calculateAverageScore(dolphinScores);
+const scoreKoalas = calculateAverageScore(koalaScores);
+
+// Determine the winner
+if (scoreDolphins > scoreKoalas) {
+    console.log("Dolphins win the trophy");
+} else if (scoreKoalas > scoreDolphins) {
+    console.log("Koalas win the trophy");
+} else {
+    console.log("Both win the trophy");
+}
